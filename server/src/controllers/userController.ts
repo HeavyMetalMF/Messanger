fa2fcd4ced6b95c37731396c5428c4dc46a1fdb9
HeavyMetalMF <condrashov20123@mail.ru>
2023-06-08 20:31:04 +0300

    сделал 2 функции по получению юзеров

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -4,19 +4,28 @@ import db from "../database/db";
 export class UserController {
     async createUser(req: Request, res: Response) {
         const {login, password, username} = req.body;
-        const newUser = await db.query("INSERT INTO users (login, password, username) values ($1, $2, $3) RETURNING *", [login, password, username])
+        const newUser = await db.query(
+            "INSERT INTO users (login, password, username) values ($1, $2, $3) RETURNING *",
+            [login, password, username]
+        )
         console.log(newUser)
         res.json(newUser.rows)
     }
 
-    // async getUsers(req: Request, res: Response) {
-    //
-    // }
-    //
-    // async getOneUser(req: Request, res: Response) {
-    //
-    // }
-    //
+    async getUsers(_: Request, res: Response) {
+        const allUsers = await db.query("SELECT * FROM users")
+        res.json(allUsers)
+    }
+
+    async getOneUser(req: Request, res: Response) {
+        const id: number = Number(req.params.id)
+        const user = await db.query(
+            "SELECT * FROM users WHERE id = $1",
+            [id]
+        )
+        res.json(user.rows)
+    }
+
     // async updateUser(req: Request, res: Response) {
     //
     // }
--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -5,8 +5,8 @@ import {UserController} from '../controllers/userController'
 const router = Router()
 const userController = new UserController()
 
-// router.get('/user', userController.getUsers)
-// router.get('/user/:id', userController.getOneUser)
+router.get('/user', userController.getUsers)
+router.get('/user/:id', userController.getOneUser)
 router.post('/user', userController.createUser)
 // router.put('/user', userController.updateUser)
 // router.delete('/user', userController.deleteUser)
